b94798dca1c98feaad2b76e8d4054f4349365338
Benjamin23115 <benjaminfarias313@gmail.com>
2024-12-09 00:03:17 -0800

    add extra java args for start script

--- startScript.sh
+++ startScript.sh
@@ -5,4 +5,4 @@ python3 /opt/minecraft/getPaperServer.py
 chmod +rwx *.jar
 
 # Run the Java command with the full path to the JAR file
-java -Dcom.mojang.eula.agree=true -Xms1G -Xmx6G -jar ./minecraftspigot.jar
+java -Dcom.mojang.eula.agree=true java -Xms20G -Xmx20G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paperclip.jar nogui -jar ./minecraftspigot.jar
